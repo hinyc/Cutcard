93eaa8886f3b185c674e1bdf9314891718d4f56d
Baeg <gye04056@gmail.com>
2021-12-15 13:53:15 +0900

    Add Common Components
    add header&nav, button, footer component

---
+++ client/src/components/Button.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const ButtonStyle = styled.button`
+  color: white;
+  outline: 0;
+  border: 0;
+  background-color: grey;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem 3rem;
+  font-size: 1rem;
+  font-weight: 400;
+  &:hover {
+    cursor: pointer;
+    opacity: 80%;
+  }
+`;
+
+function Button({ text }) {
+  return <ButtonStyle>{text}</ButtonStyle>;
+}
+
+export default Button;
