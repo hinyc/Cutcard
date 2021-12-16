e28d3317044864916dc39f175345bb4f88ba7812
hinyc <dlsduf@gmail.com>
2021-12-16 10:00:24 +0900

    오타수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import React from "react";
-import Button from "./components/Button";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import Input from "./components/Input";
+import React from 'react';
+import Button from './components/Button';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Input from './components/Input';
 
-import { MainOutcomes } from "./components/MainOutcomes";
-import { MainIncomes } from "./components/MainIncomse";
-import "./App.css";
-import { useState } from "react";
-import { Calendar } from "./components/Calendar";
+import { MainOutcomes } from './components/MainOutcomes';
+import { MainIncomes } from './components/MainIncomes';
+import './App.css';
+import { useState } from 'react';
+import { Calendar } from './components/Calendar';
 
 function App() {
-  const [mainState, setMainState] = useState("income");
+  const [mainState, setMainState] = useState('income');
 
   const mainStateHandler = (target) => {
     setMainState(target);
@@ -20,20 +20,9 @@ function App() {
   return (
     <>
       <Navbar />
-      <Button text={"버튼"} />
-      <Input
-        label={"라벨"}
-        type={"text"}
-        text={"플레이스홀더"}
-        readOnly={false}
-      />
-      <div className="App">
-        {mainState === "outcome" ? (
-          <MainOutcomes mainStateHandler={mainStateHandler} />
-        ) : (
-          <MainIncomes mainStateHandler={mainStateHandler} />
-        )}
-      </div>
+      <Button text={'버튼'} />
+      <Input label={'라벨'} type={'text'} text={'플레이스홀더'} readOnly={false} />
+      <div className="App">{mainState === 'outcome' ? <MainOutcomes mainStateHandler={mainStateHandler} /> : <MainIncomes mainStateHandler={mainStateHandler} />}</div>
       <Footer />
     </>
   );
