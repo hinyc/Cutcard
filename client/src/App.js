4af4b2e9291d9a3ced98f346299599fae660a685
Baeg <gye04056@gmail.com>
2021-12-17 11:09:00 +0900

    change

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,14 @@
 import React from "react";
-import { SmallButton, BigButton } from "./components/Button";
+
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import { Input, LoginInput } from "./components/Input";
-import { CardList, AddCard } from "./components/CardList";
-
-import { MainOutcomes } from "./components/MainOutcomes";
-import { MainIncomes } from "./components/MainIncomes";
-import "./App.css";
-import { useState } from "react";
-import { Calendar } from "./components/Calendar";
+import MainPage from "./pages/MainPage";
 
 function App() {
-  const [mainState, setMainState] = useState("income");
-
-  const mainStateHandler = (target) => {
-    setMainState(target);
-  };
-
   return (
     <>
       <Navbar />
-      <SmallButton text={"작은 버튼"} />
-      <BigButton text={"큰 버튼"} />
-      <Input
-        label={"라벨"}
-        type={"text"}
-        text={"플레이스홀더"}
-        readOnly={false}
-      />
-      <LoginInput
-        label={"로그인"}
-        type={"text"}
-        text={"플레이스홀더"}
-        readOnly={false}
-      />
-      <CardList text={"신한카드"} />
-      <AddCard />
-
-      <div className="App">
-        {mainState === "outcome" ? (
-          <MainOutcomes mainStateHandler={mainStateHandler} />
-        ) : (
-          <MainIncomes mainStateHandler={mainStateHandler} />
-        )}
-      </div>
+      <MainPage />
       <Footer />
     </>
   );
