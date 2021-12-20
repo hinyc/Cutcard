6bf59e2484362578799822b8e7ce6661f67f2af2
hinyc <dlsduf@gmail.com>
2021-12-20 22:49:02 +0900

    APItest

--- server/controllers/transaction/outcomes.js
+++ server/controllers/transaction/outcomes.js
@@ -1,30 +1,36 @@
 const { transactions, userCards } = require('./../../models');
-const { isAuthorized } = require('./../tokenFunctions')
+const { isAuthorized } = require('./../tokenFunctions');
 
 module.exports = async (req, res) => {
   // accessToken 확인
+  console.log('옜다응답!');
+
   const accessTokenData = isAuthorized(req, res);
-  if(!accessTokenData) {
-    return res.status(401).json({ data: null, message: "invalid access token!" })
+  if (!accessTokenData) {
+    console.log('토큰없다!!');
+    return res.status(401).json({ data: null, message: 'invalid access token!' });
   } else {
     const { id } = accessTokenData;
     const { year, month, day, category, outcomeIsCash, userCardId, price, isIncome } = req.body;
-    let userCard
-    if(!outcomeIsCash && userCardId !== undefined) {
+    let userCard;
+    if (!outcomeIsCash && userCardId !== undefined) {
       userCard = await userCards.findOne({
         where: {
-          cardId: userCardId
-        }
-      })
+          cardId: userCardId,
+        },
+      });
       userCard.dataValues.remainValue += price;
-      await userCards.update({
-        remainValue: userCard.dataValues.remainValue
-      },{
-        where: {
-          cardId: userCardId
+      await userCards.update(
+        {
+          remainValue: userCard.dataValues.remainValue,
+        },
+        {
+          where: {
+            cardId: userCardId,
+          },
         }
-      })
-      if(month === 12) {
+      );
+      if (month === 12) {
         await transactions.create({
           year: year + 1,
           month: 1,
@@ -34,17 +40,17 @@ module.exports = async (req, res) => {
           isIncome,
           outcomeIsCash: outcomeIsCash,
           userId: id,
-          userCardId: userCard.dataValues.cardId
-        })
+          userCardId: userCard.dataValues.cardId,
+        });
         const outcomeData = await transactions.findAll({
           where: {
             year,
             month,
             userId: id,
-            isIncome
-          }
-        })
-        res.status(201).json({ transaction: outcomeData })
+            isIncome,
+          },
+        });
+        res.status(201).json({ transaction: outcomeData });
       } else {
         await transactions.create({
           year,
@@ -55,17 +61,17 @@ module.exports = async (req, res) => {
           isIncome,
           outcomeIsCash: outcomeIsCash,
           userId: id,
-          userCardId: userCard.dataValues.cardId
-        })
+          userCardId: userCard.dataValues.cardId,
+        });
         const outcomeData = await transactions.findAll({
           where: {
             year,
             month,
             userId: id,
-            isIncome
-          }
-        })
-        res.status(201).json({ transaction: outcomeData })
+            isIncome,
+          },
+        });
+        res.status(201).json({ transaction: outcomeData });
       }
     } else {
       await transactions.create({
@@ -75,21 +81,21 @@ module.exports = async (req, res) => {
         category,
         price,
         isIncome,
-        outcomeIsCash: outcomeIsCash, 
+        outcomeIsCash: outcomeIsCash,
         userId: id,
         userCardId: null,
         createdAt: new Date(),
-        updatedAt: new Date()
-      })
+        updatedAt: new Date(),
+      });
       const outcomeData = await transactions.findAll({
         where: {
           year,
           month,
           userId: id,
-          isIncome
-        }
-      })
-      res.status(201).json({ transaction: outcomeData })
+          isIncome,
+        },
+      });
+      res.status(201).json({ transaction: outcomeData });
     }
   }
-}
+};
